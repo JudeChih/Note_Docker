40fa41e88616e9be74f8a5ff46d13081309046c0
黃丞志(Toby) <sausage760703@gmail.com>
2019-10-24 14:09:41 +0800

    第一次上傳

---
+++ click.bat
@@ -0,0 +1,4 @@
+MOVE /Y C:\Users\Toby_huang\Desktop\dockerSettingFiles\.env C:\Users\Toby_huang\laradock
+MOVE /Y C:\Users\Toby_huang\Desktop\dockerSettingFiles\default.conf C:\Users\Toby_huang\laradock\nginx\sites
+MOVE /Y C:\Users\Toby_huang\Desktop\dockerSettingFiles\php7.2.ini C:\Users\Toby_huang\laradock\php-fpm
+MOVE /Y C:\Users\Toby_huang\Desktop\dockerSettingFiles\my.cnf C:\Users\Toby_huang\laradock\mysql
